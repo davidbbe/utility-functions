526a564c0a6c822e0d6ea61e916fe79391ea1f53
davidbbe <david123beauchamp@gmail.com>
2022-04-22 16:53:50 +0400

    Adding document script injection React hook

---
+++ injecting-scripts-react-effect.js
@@ -0,0 +1,99 @@
+// useScript.js
+// Add a script to the site during a useEffect hook
+import { useState, useEffect } from 'react';
+
+export function useScriptSrc(src) {
+	// https://usehooks.com/useScript/
+	// Keep track of script status ("idle", "loading", "ready", "error")
+	const [status, setStatus] = useState(src ? 'loading' : 'idle');
+
+	useEffect(() => {
+		// Allow falsy src value if waiting on other data needed for
+		// constructing the script URL passed to this hook.
+		if (!src) {
+			setStatus('idle');
+			return;
+		}
+
+		// Fetch existing script element by src
+		// It may have been added by another intance of this hook
+		let script = document.querySelector(`script[src="${src}"]`);
+
+		if (!script) {
+			script = document.createElement('script');
+			script.src = src;
+			script.async = true;
+			script.setAttribute('data-status', 'loading');
+			document.body.appendChild(script);
+
+			// Store status in attribute on script
+			// This can be read by other instances of this hook
+			const setAttributeFromEvent = (event) => {
+				script.setAttribute(
+					'data-status',
+					event.type === 'load' ? 'ready' : 'error'
+				);
+			};
+
+			script.addEventListener('load', setAttributeFromEvent);
+			script.addEventListener('error', setAttributeFromEvent);
+		} else {
+			// Grab existing script status from attribute and set to state.
+			setStatus(script.getAttribute('data-status'));
+		}
+
+		// Script event handler to update status in state
+		// Note: Even if the script already exists we still need to add
+		// event handlers to update the state for *this* hook instance.
+		const setStateFromEvent = (event) => {
+			setStatus(event.type === 'load' ? 'ready' : 'error');
+		};
+
+		// Add event listeners
+		script.addEventListener('load', setStateFromEvent);
+		script.addEventListener('error', setStateFromEvent);
+
+		// Remove event listeners on cleanup
+		return () => {
+			if (script) {
+				script.removeEventListener('load', setStateFromEvent);
+				script.removeEventListener('error', setStateFromEvent);
+				script.parentElement.removeChild(script);
+			}
+		};
+	}, [src]);
+
+	return status;
+}
+
+export function useScriptCode(code, id) {
+	const [isReady, setIsReady] = useState(false);
+
+	useEffect(() => {
+		if (!code || !id) {
+			return;
+		}
+
+		// Fetch existing script element by id
+		// It may have been added by another intance of this hook
+		let script = document.getElementById(id);
+
+		if (!script) {
+			script = document.createElement('script');
+			script.innerHTML = code;
+			script.id = id;
+			script.async = true;
+			document.body.appendChild(script);
+			setIsReady(true);
+		}
+
+		// Remove script on cleanup
+		return () => {
+			if (script) {
+				script.parentElement.removeChild(script);
+			}
+		};
+	}, [code]);
+
+	return isReady;
+}
